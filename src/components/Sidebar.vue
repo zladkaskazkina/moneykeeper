841294676c514839ba662faa13d386d8e890f249
Vladi Benesova <vladi.benesova1@gmail.com>
2020-08-04 20:48:34 +0200

    change sidebar into router

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,19 +1,43 @@
 <template>
   <ul class="sidenav app-sidenav open">
-    <li>
-      <a href="#" class="waves-effect waves-orange pointer">Detail</a>
-    </li>
-    <li>
-      <a href="#" class="waves-effect waves-orange pointer">History</a>
-    </li>
-    <li>
-      <a href="#" class="waves-effect waves-orange pointer">Planning</a>
-    </li>
-    <li>
-      <a href="#" class="waves-effect waves-orange pointer">New record</a>
-    </li>
-    <li>
-      <a href="#" class="waves-effect waves-orange pointer">Categories</a>
-    </li>
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+    </router-link>
   </ul>
 </template>
+<script>
+export default {
+  data: () => ({
+    links: [
+      {
+        title: "Detail",
+        url: "/",
+        exact: true
+      },
+      {
+        title: "History",
+        url: "/history"
+      },
+      {
+        title: "Planning",
+        url: "/planning"
+      },
+      {
+        title: "New record",
+        url: "/record"
+      },
+      {
+        title: "Categories",
+        url: "/categories"
+      }
+    ]
+  })
+};
+</script>
